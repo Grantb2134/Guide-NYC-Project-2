556095ea98873942c84919fa6ebcd96bd205e91b
JasonRolle1990 <89821605+JasonRolle1990@users.noreply.github.com>
2021-11-14 23:17:36 -0500

    modified project

--- nyc-guide-main/nyc/boroughs.py
+++ nyc-guide-main/nyc/boroughs.py
@@ -1,119 +1,231 @@
 boroughs = {
     'brooklyn': {
         'beaches': {
-            'brighton beach': { 
+            'brighton beach': [{ 
+                'hdr':'Sunset on the Water',
                 'img_link': 'https://assets3.thrillist.com/v1/image/2829155/1200x630/flatten;crop_down;jpeg_quality=70',
                 'description': 'Also known as "Little Odessa" due its tight-knit Russian and Eastern European communities, Brooklyn\'s Brighton Beach is a lively neighborhood with many high-rise residential buildings. \ Traditional ethnic restaurants and food markets line Brighton Beach Avenue. The beach and boardwalk here are more laid-back than nearby Coney Island, catering largely to locals. Splashy nightclubs attract partiers in the evenings.'
             },
-            'coney island': {
+            {
+                'hdr':'Another Day in Paradise',
+                'img_link':'https://www.tripsavvy.com/thmb/_LBSrwtbs3t3kswIDo6eacL6UAk=/2309x1299/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-518852688-591cabdd5f9b58f4c0e0e228.jpg'
+            }],
+            'coney island': [{
+                'hdr':'Carnival On the Beach',
                 'img_link': 'https://media.timeout.com/images/105763396/image.jpg',
                 'description': 'Coney Island is a residential Brooklyn neighborhood that morphs into a relaxation and entertainment destination each summer. Locals and tourists crowd its beach, the Wonder Wheel and Luna Park, an amusement park featuring the famed Cyclone roller coaster. Street performers, the Circus Sideshow and the Mermaid Parade in June lend an eccentric vibe. Nathan\'s Famous is known for its July 4th hot-dog eating contest.'
             },
-            'manhattan beach': {
-                'img_link': 'https://easyreadernews.com/wp-content/uploads/2019/07/mb-discover.jpeg',
+            {
+                'hdr':'Buckle up, its a ride you cant stop!',
+                'img_link':'https://lp-cms-production.imgix.net/2021-01/Wonder%20Wheel_1.jpg'
+            }],
+            'manhattan beach': [{
+                'hdr':'Sunset Surf',
+                'img_link': 'https://images.contentstack.io/v3/assets/blt00454ccee8f8fe6b/blt08bc30a9fac65257/605c090dc4f5970f345859b8/US_ManhattanBeach_US_Header.jpg',
                 'description': 'Manhattan Beach is a laid-back South Bay community popular with families and outdoor enthusiasts. The Strand bike path winds along the oceanfront between modern mansions and wide stretches of sand lined with volleyball courts. Low-key Manhattan Beach Pier features a quaint aquarium and views of surfers. There are several parks, and a small downtown area with boutiques, brunch cafes, pubs & creative, upscale eateries.'
-            }
+            },
+            {
+                'hdr':'Manhattans Pier Mystiscism',
+                'img_link':'https://i0.wp.com/laflyrides.com/wp-content/uploads/2016/12/8b4db8c8c5df8cbbdd32adaa9143c2a3.jpg?resize=646%2C484'
+            }],
         }
     },
     'bronx': {
         'beaches': {
-            'orchard beach': {
+            'orchard beach': [{
+                'hdr':'Vitamin D for Free',
                 'img_link': 'https://live.staticflickr.com/4116/4852943653_c3f5d25d8f_b.jpg',
                 'description': 'Orchard Beach, Bronx\'s sole public beach, was proclaimed "The Riviera of New York" when it was created in the 1930s. The 115-acre, 1.1-mile-long beach contains a hexagonal-block promenade, a central pavilion, snack bars, food and souvenir carts, two playgrounds, two picnic areas, a large parking lot, and 26 courts for basketball, volleyball, and handball. Changing areas and showers are available.'
-            }
+            },
+            {
+                'hdr':'Cloud 9',
+                'img_link':'https://offloadmedia.feverup.com/secretnyc.co/wp-content/uploads/2019/01/10131948/orchard-beach.jpg'
+            }]
         },
         'zoos': {
-            'bronx zoo': {
+            'bronx zoo': [{
+                'hdr':'Natures Best',
                 'img_link': 'https://i.ytimg.com/vi/zYTf1l71z_M/hqdefault.jpg',
                 'description': 'The Bronx Zoo is a zoo located within Bronx Park in the Bronx, New York. It is one of the largest zoos in the United States by area, and is the largest metropolitan zoo in the United States by area, comprising 265 acres of park lands and naturalistic habitats separated by the Bronx River.'
-            }
+            },
+            {
+                'hdr':'On the prowl',
+                'img_link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3lAsbx7_--oHYYhiMwxS91VRJYTM4SciX4Q&usqp=CAU'
+            }],
         }
     },
     'manhattan': {
         'food': {
-            'cheeky sandwiches': {
-                'img_link': 'https://www.nyrestaurantsguide.com/wp-content/uploads/img/sandwiches/cheeky/cheeky_sandwiches_new_york_menu_1-600x800.jpg',
+            'cheeky sandwiches': [{
+                'hdr':'Menu',
+                'img_link': 'https://www.carpecity.com/wp-content/uploads/2018/04/cheeky-sandwiches-inside-carpe-city.jpg',
                 'description': 'A hip sandwich shop offering a select menu of po’ boys & other Big Easy bites in a tiny storefront.'
-            },
-            'katz\'s delicatessen': {
-                'img_link': 'https://i.etsystatic.com/17697936/r/il/b000ce/3201123009/il_794xN.3201123009_84s8.jpg',
+            },{
+                'hdr':'Welcome',
+                'img_link': 'https://www.carpecity.com/wp-content/uploads/2018/04/cheeky-sandwiches-outside-carpe-city.jpg'
+            }],
+            'katz\'s delicatessen': [{
+                'hdr':'Everything slows down at Katz\'s',
+                'img_link': 'https://d2zyb4ugwufqpc.cloudfront.net/media/wysiwyg/web_2_1.jpg',
                 'description': 'No-frills deli with theatrically cranky service serving mile-high sandwiches since 1888.'
             },
-            'veselka': {
+            {
+                'hdr':'World famous pastrami and corned beef combo',
+                'img_link':'https://cdn.vox-cdn.com/thumbor/W3STwCzJUEXm92t_IeKgvh7A3qI=/0x0:2048x1536/920x0/filters:focal(0x0:2048x1536):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/19098071/KatzTheHack.jpg'
+            }],
+            'veselka': [{
+                'hdr':'Grab a quick bite',
                 'img_link': 'https://www.kyivpost.com/wp-content/uploads/2018/12/Veselka-interior-2-800x520.jpg',
                 'description': 'Borscht & pierogi are menu highlights at this cheap, no-frills Ukrainian eatery open 24 hours a day.'
-            }
+            },
+            {
+                'hdr':'Classic NYC Eatery',
+                'img_link':'https://untappedcities.com/wp-content/uploads/2016/08/VeselkafromSFII-jamesandmurrayphotography-nyc-untappedcitied.jpg'
+            }]
         },
         'parks': {
-            'central park': {
+            'central park': [{
+                'hdr':'Autumn\'s Paradise',
                 'img_link': 'https://www.planetware.com/photos-large/USNY/new-york-city-central-park-1.jpg',
                 'description': 'Central Park is an urban park in New York City located between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city by area, covering 843 acres.'
             },
-            'riverside park': {
+            {
+                'hdr':'Scene to Remember',
+                'img_link':'https://cdn.thecrazytourist.com/wp-content/uploads/2019/02/ccimage-shutterstock_604451678.jpg'
+            }],
+            'riverside park': [{
+                'hdr':'Tony Hawk\'s Playground',
                 'img_link': 'https://livingnewdeal.org/wp-content/uploads/2015/08/riverside34-r.jpg',
                 'description': 'Riverside Park is a scenic waterfront public park in the Upper West Side, Morningside Heights, and Hamilton Heights neighborhoods of the borough of Manhattan in New York City.'
             },
-            'the highline':  {
+            {
+                'hdr':'The Podium',
+                'img_link':'https://www.nycgo.com/images/venues/5965/riverside-park-alexander-thompson-24__large.jpg'
+            }],
+            'the highline': [ {
+                'hdr':'A Walk to Remember',
                 'img_link': 'http://eportfolios.macaulay.cuny.edu/nycpublicspaces/files/2015/05/article-2139899-12EF2F80000005DC-992_964x1437.jpg',
                 'description': 'The High Line is a 1.45-mile-long elevated linear park, greenway and rail trail created on a former New York Central Railroad spur on the west side of Manhattan in New York City. The High Line\'s design is a collaboration between James Corner Field Operations, Diller Scofidio + Renfro, and Piet Oudolf.'
             },
-            'washington square park':  {
+            {
+                'hdr':'Walking Into the Future',
+                'img_link':'https://www.newyorkbyrail.com/wp-content/uploads/2018/04/The-High-Line-NYC-NY-New-York-By-Rail-Francois-Roux.jpg'
+            }],
+            'washington square park':  [{
+                'hdr':'Where all walk\'s of life meet',
                 'img_link': 'https://i.pinimg.com/originals/46/ac/ae/46acaefc5e243415deb7badb29b4113b.jpg',
                 'description': 'Washington Square Park is a 9.75-acre public park in the Greenwich Village neighborhood of Lower Manhattan, New York City. One of the best known of New York City\'s public parks, it is an icon as well as a meeting place and center for cultural activity.'
-            }
+            },
+            {
+                'hdr':'Concrete Jungle',
+                'img_link':'http://2.bp.blogspot.com/_HUjYSMsnxvc/S8rDLI099DI/AAAAAAAADfc/UUDP4HJf7II/s1600/%21.jpg'
+            }],
         }
     },
     'queens': {
         'airports': {
-            'john f kennedy international airport': {
-                'img_link': 'https://media.cntraveler.com/photos/580797286f39a9b41381a24f/16:9/w_2560%2Cc_limit/EB7YHM.jpg',
+            'john f kennedy international airport': [{
+                'hdr':'The Future\'s Airport',
+                'img_link': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqxceeJdUeR2RWkYEGYi6IMu8mj-NGMzEv_w&usqp=CAU',
                 'description': 'John F. Kennedy International Airport is an international airport in Queens, New York, USA. It is the primary international airport serving New York City.'
             },
-            'laguardia airport': {
-                'img_link': 'https://www.usa.skanska.com/globalassets/externalcontent2/project/laguardia-airport---central-terminal-b-redevelopment/28ea3a55-3cea-4a89-afa8-29a6c0930b8a.4.jpg',
+            {
+                'hdr':'International Glow',
+                'img_link':'https://media.architecturaldigest.com/photos/586e9ca80add084375e9034f/16:9/w_2560%2Cc_limit/JFK-airport-001.jpg'
+            }],
+            'laguardia airport': [{
+                'hdr':'Airport sign',
+                'img_link': 'https://upgradedpoints.com/wp-content/uploads/2019/07/laguardia-lga-airport.jpg',
                 'description': 'LaGuardia Airport is an airport in Queens, New York. The airport is the third busiest airport serving New York City, and the twentieth busiest in the United States. LaGuardia Airport covers 680 acres.'
-            }
+            },
+            {
+                'hdr':'Futuristic Airway',
+                'img_link':'https://www.usa.skanska.com/globalassets/externalcontent2/project/laguardia-airport---central-terminal-b-redevelopment/28ea3a55-3cea-4a89-afa8-29a6c0930b8a.4.jpg'
+            }]
         },
         'beaches': {
-            'rockaway beaches': {
-                'img_link': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Rockaway_Beach_Queens.JPG/1200px-Rockaway_Beach_Queens.JPG',
-                'description': 'From surfers to swimmers to the Ramones, everyone wants to "hitch a ride to Rockaway Beach." Rockaway Beach is a year-round resource for residents all along the Rockaway peninsula, and it comes alive each summer with millions of visitors. Beach goers can enjoy the sand and water, the variety of concessions, and the city\'s only legal surfing beaches. Rockaway Beach is also home to a variety of playgrounds and other outdoor activities.'
-            }
+            'rockaway beaches':[ 
+                {
+                    'hdr': 'One of the Worlds Funnest Beaches',
+                    'img_link': 'https://websterapartments.org/wp-content/uploads/coneyisland.jpg',
+                    'description': 'From surfers to swimmers to the Ramones, everyone wants to "hitch a ride to Rockaway Beach." Rockaway Beach is a year-round resource for residents all along the Rockaway peninsula, and it comes alive each summer with millions of visitors. Beach goers can enjoy the sand and water, the variety of concessions, and the city\'s only legal surfing beaches. Rockaway Beach is also home to a variety of playgrounds and other outdoor activities.'
+                },
+                {
+                    'hdr': 'Energy',
+                    'img_link': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Rockaway_Beach_Queens.JPG/1200px-Rockaway_Beach_Queens.JPG'
+                }
+            
+            ]
         }
 
     },
     'staten island': {
         'beaches': {
-            'cedar grove beach': {
-                'img_link': 'https://fastly.4sqi.net/img/general/600x600/155927_NFm7tFEB6LiNRAGS1W3bE8kVA4A8odlH_9GNEaJZL54.jpg',
+            'cedar grove beach': [{
+                'hdr':'Sign',
+                'img_link': 'https://fastly.4sqi.net/img/general/600x600/60127825_8XXI3YXQHzpRz4cyqwMpiSDdOxfKMEQvb8XEv6q0JXg.jpg',
                 'description': 'The city\'s newest beach is a converted oceanfront bungalow colony. This charming beach is smaller and somewhat more tranquil than its neighbors, Midland Beach and South Beach, and is popular with the area\'s families.'
-            },
-            'midland beach': {
+                },
+            {
+                'hdr':'Peace',
+                'img_link':'https://cdn.vox-cdn.com/thumbor/FT6W30EqdPR0AzM-HSV5djgPWys=/0x0:1024x765/1200x900/filters:focal(431x301:593x463):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62532867/Cedar_Grove_Beach.0.0.0.0.0.jpg'
+            }],
+            'midland beach': [{
+                'hdr':' Break a Sweat',
                 'img_link': 'https://images.nycgo.com/image/fetch/q_auto:eco,c_fill,f_auto,w_780,g_north/https://www.nycgo.com/images/venues/2978/img_6464_crop.jpg',
                 'description': 'Midland Beach shares the Franklin D. Roosevelt Boardwalk with neighboring South Beach, and tends to be the somewhat less crowded of the two beaches. The beach\'s attractions include a playground and courts for tennis and shuffleball, as well as a sea turtle fountain for children.'
             },
-            'south beach': {
+            {
+                'hdr':'Scenery at its Best',
+                'img_link':'https://www.nycgovparks.org/facilities/images/beaches/south-beach-content.jpg'
+            }],
+            'south beach': [{
+                'hdr':'Safet First',
                 'img_link': 'https://i2.wp.com/www.statenislandnycliving.com/wp-content/uploads/2014/08/SouthBeach_V1_460x285.jpg?w=640',
                 'description': 'Beachgoers can sunbathe while taking in a lovely view of the Verazzano Bridge, bike through a scenic trail, jog along the boardwalk, kayak, play tennis, or fish off of the Ocean Breeze Fishing Pier, one of the city\'s most popular. A park on the beach designed especially for seniors contains chess tables, benches, and bocce courts. The beach\'s Fountain of Dolphins is a popular stop on any tour of the boardwalk.'
             },
-            'wolfe\'s pond beach': {
-                'img_link': 'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.silive.com/home/silive-media/width2048/img/latest_news/photo/da4-2385-06-23-14-wolfes-pondjpg-5b07bad4bbe93ab9.jpg',
+            {
+                'hdr':'Old Time Gem',
+                'img_link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSglbjZQQxagQoglR27ZoLgUf-rcIMHA90Zaw&usqp=CAU'
+            }],
+            'wolfe\'s pond beach': [{
+                'hdr':'Camping Anyone',
+                'img_link': 'https://www.nycgovparks.org/photo_gallery/full_size/23221.jpg',
                 'description': 'Love the ocean but think NYC’s beaches are just too chaotic? Wolfe’s Pond Beach is the city’s best-kept secret, a small, calm, and secluded beach that’s a perfect retreat for those looking to get away from summer’s crowds.'
-            }
+            },
+            {
+                'hdr':'Wolfe Pier',
+                'img_link':'https://www.nycgovparks.org/facilities/images/beaches/wolfes-pond-beach-content.jpg'
+            }],
         },
         'food': {
-            'lee\'s tavern': {
+            'lee\'s tavern': [{
+                'hdr':'Have a Blast, Nights creates Memories',
                 'img_link': 'https://www.silive.com/resizer/sGHVwyB6OMdXAZkEeyEqHyproc0=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/JVXTK5ABSRFF5C6PNICOCP2IH4.jpg',
                 'description': 'Family-run neighborhood pub with photos on the wall serving a range of pizza & brews.'
             },
-            'ralph\'s famous italian ices': {
+            {
+                'hdr':'Lees Famous Bar & Pizza',
+                'img_link':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSpfywG-z6g1hv1kYJ5paJI9OFbW6cHZCt9_GTL46z2i5tAGzWNmPSdwDb3BEYWUuR0rU&usqp=CAU'
+            }],
+            'ralph\'s famous italian ices': [{
+                'hdr':'Famous Ice',
                 'img_link': 'https://media-cdn.tripadvisor.com/media/photo-s/06/51/b8/d3/ralphs-in-syosset-ny.jpg',
                 'description': 'Staten Island-born chain dispensing Italian ices & ice cream in 100-plus flavors since 1928.'
             },
-            'royal crown bakery': {
+            {
+                'hdr':'Famous Flavors',
+                'img_link':'https://media-cdn.tripadvisor.com/media/photo-s/0e/a8/8e/ce/photo0jpg.jpg'
+            }],
+            'royal crown bakery': [{
+                'hdr':'NY Famous Cafe',
                 'img_link': 'https://fastly.4sqi.net/img/general/600x600/76203083_uJuzwF4ZGft6jBGvV93V4bQJSAhyml8yJZ0RwHuW-SU.png',
                 'description': 'A local Italian bakery serves bread, pastries & coffees, with an adjacent cucina for hot meals.'
-            }
+            },
+            {
+                'hdr':'World Famous Deserts',
+                'img_link':'https://s3-media0.fl.yelpcdn.com/bphoto/2EV1u3JqZI0JHb-G58svjA/300s.jpg'
+            }],
         }
     },
 }
